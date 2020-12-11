a3d0842def6c94993e32815fb2b80a433ed21ddb
SNTPG <sntpg@ucla.edu>
2020-12-10 20:46:44 -0800

    Added more API features related to pulls

--- backend/mysite/Comic/urls.py
+++ backend/mysite/Comic/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('search/<str:query>', views.search),
     path('browse', views.browse),
-    path('get/<str:query>', views.get)
+    path('get/<str:query>', views.get),
+    path('add/<str:person>/<str:query>', views.add),
+    path('remove/<str:person>/<str:query>', views.remove),
+    path('pulls/<str:person>', views.pulls)
 ]
--- backend/mysite/Comic/views.py
+++ backend/mysite/Comic/views.py
@@ -1,6 +1,7 @@
 import csv,io,os,datetime
 from django.http import HttpResponse
 from .models import Comic
+from authentication.models import User
 from . import views
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -9,12 +10,13 @@ def search(request, query):
     comics = Comic.objects.filter(full_title__contains=words[0])
     for word in words:
         comics = comics.filter(full_title__contains=word)
-    json = '[ '
+    json = '<pre>[  '
     for comic in comics:
         json += str(comic)
-        json += ","
-    json = json[:-1]
-    json += "]"
+        json += ",\n"
+    json = json[:-2]
+    json += "]</pre>"
+    json.replace('\n','<br/>')
     return HttpResponse(json)
 
 def browse(request):
@@ -25,24 +27,60 @@ def browse(request):
     dateString = str(nextRelease.month)+'/'+str(nextRelease.day)+'/'+str(nextRelease.year) # Because the sqlite database got a bit screwed
     #dateString = nextRelease.strftime("%Y-%m-%d")
     comics = Comic.objects.filter(ship_date=dateString).exclude(oa='Y')#.exclude(mature='Y')
-    json = '[ '
+    json = '<pre>[  '
     for comic in comics:
         json += str(comic)
-        json += ","
-    json = json[:-1]
-    json += "]"
+        json += ",\n"
+    json = json[:-2]
+    json += "]</pre>"
+    json.replace('\n','<br/>')
     return HttpResponse(json)
 
 def get(request, query):
     words = query.split("-")
-    json = '[ '
+    json = '<pre>[  '
     for word in words:
         try:
             comic = Comic.objects.get(stock_no=word)
             json += str(comic)
-            json += ","
+            json += ",\n"
         except ObjectDoesNotExist:
             continue
-    json = json[:-1]
-    json += "]"
+    json = json[:-2]
+    json += "]</pre>"
+    json.replace('\n','<br/>')
     return HttpResponse(json)
+
+def add(request, person, query):
+    try:
+        user = User.objects.get(username = person)
+    except ObjectDoesNotExist:
+        return HttpResponse("User does not exist!")
+    words = query.split("-")
+    for word in words:
+        if user.pull_list == "":
+            user.pull_list += word
+        else:
+            user.pull_list += ("-" + word)
+    user.save()
+    return HttpResponse("Pull list successfully updated to: " + user.pull_list)
+
+def remove(request, person, query):
+    try:
+        user = User.objects.get(username = person)
+    except ObjectDoesNotExist:
+        return HttpResponse("User does not exist!")
+    words = query.split("-")
+    for word in words:
+        user.pull_list = user.pull_list.replace('-'+word, '')
+        user.pull_list = user.pull_list.replace(word+'-', '')
+        user.pull_list = user.pull_list.replace(word, '')
+    user.save()
+    return HttpResponse("Pull list successfully updated to: " + user.pull_list)
+
+def pulls(request, person):
+    try:
+        user = User.objects.get(username = person)
+    except ObjectDoesNotExist:
+        return HttpResponse("User does not exist!")
+    return get(request, user.pull_list)
