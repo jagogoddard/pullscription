cb0f4006359783a386274655aae36a99789df9c6
SNTPG <sntpg@ucla.edu>
2020-12-20 21:10:16 -0800

    Fixed rollback issue with Django URLs

--- backend/mysite/mysite/urls.py
+++ backend/mysite/mysite/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    #path('api/comics/', include('Comic.urls')),
+    path('api/comics/', include('Comic.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('authentication.urls')),
