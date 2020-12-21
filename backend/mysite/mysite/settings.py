3b0d15c8c85efd58417b91760e16a7d14d50ca7c
SNTPG <sntpg@ucla.edu>
2020-12-20 21:22:03 -0800

    Version error; uncommitted app installation fixed

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'Comic',
+    'Comic',
     'rest_framework',
     'drf_yasg',
     'authentication',
